ef92f4ee738b8e584125e320b67fd72cb9d90612
Max Gleeson <mgleeson@sandstormdesign.com>
2019-07-19 12:00:22 -0500

    Remove unused 'nmma' theme

--- docroot/themes/custom/nmma/js/src/4-organisms/forms.js
+++
@@ -1,9 +0,0 @@
-(function ($, Drupal) {
-  Drupal.behaviors.formActions = {
-    attach: function attach(context, settings) {
-      if ($(context)[0].id == "views-exposed-form-videos-video-display-grid") {
-        $(".block-views-blockvideos-featured-video-block").slideUp();
-      }
-    }
-  };
-})(jQuery, Drupal);
