1a59550f18272343d09dde3103b84a7dd2e1ee1b
jruel <jruel@sandstormdesign.com>
2019-07-19 14:24:45 -0500

    fixed drupalvm vagrant nfs seting to work with gulp watch, cleaned up watch task

--- docroot/themes/custom/boatshow/gulp-tasks/watch.js
+++ docroot/themes/custom/boatshow/gulp-tasks/watch.js
@@ -8,29 +8,14 @@
 module.exports = function (gulp, plugins, options) {
   'use strict';
 
-  gulp.task('watch', ['watch:sass', 'watch:styleguide', 'watch:js', 'watch:lint']);
+  gulp.task('watch', ['watch:sass', 'watch:js', 'watch:lint']);
 
   gulp.task('watch:js', function () {
-    return gulp.watch([
-      options.js.files
-    ], function () {
-      plugins.runSequence(
-        'compile:js',
-        'browser-sync:reload'
-      );
-    });
+    gulp.watch([options.js.files], ['compile:js']);
   });
 
   gulp.task('watch:sass', function () {
-    return gulp.watch([
-      options.sass.files
-    ], function () {
-      plugins.runSequence(
-        'compile:sass',
-        'minify:css',
-        'browser-sync:reload'
-      );
-    });
+    gulp.watch([options.sass.files], ['compile:sass']);
   });
 
   gulp.task('watch:styleguide', function () {
@@ -44,15 +29,7 @@ module.exports = function (gulp, plugins, options) {
   });
 
   gulp.task('watch:lint', function () {
-    return gulp.watch([
-      options.sass.files,
-      options.js.files
-    ], function () {
-      plugins.runSequence(
-        'lint:css',
-        'lint:js'
-      );
-    });
+    gulp.watch([options.sass.files, options.js.files], ['lint:css', 'lint:js']);
   });
 
   var connect = require('gulp-connect');
