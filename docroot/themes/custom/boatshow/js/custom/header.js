53e1fe9d4a59ea716be1b1509cf25fc626fd525d
jruel <jruel@sandstormdesign.com>
2019-07-29 15:56:38 -0500

    cleaned up interactions and consolidated header and nav js into one for clarity's sake

--- docroot/themes/custom/boatshow/js/custom/header.js
+++ docroot/themes/custom/boatshow/js/custom/header.js
@@ -4,34 +4,7 @@ window.BoatShows = window.BoatShows || {};
   'use strict';
 
   var Header = function() {
-    var $preHeader, $header, $headerBar, $contentRegion, headerOffset, $thisContext, $window;
-
-    Drupal.behaviors.boatShowHeader = {
-      attach: function (context, settings) {
-        $thisContext = $(context);
-        updateState();
-        $window = $(window, context);
-
-        if ($window.scrollTop() >= headerOffset) {
-          $header.addClass('fixed-header');
-          $contentRegion.css('margin-top', $header.outerHeight());
-        }
-        else {
-          $window.scroll(function() {
-            var $thisWindow = $(this);
-
-            if ($thisWindow.scrollTop() >= headerOffset && !$header.hasClass('fixed-header')) {
-              $header.addClass('fixed-header');
-              $contentRegion.css('margin-top', $header.outerHeight());
-            }
-            else if ($thisWindow.scrollTop() < headerOffset && $header.hasClass('fixed-header')) {
-              $header.removeClass('fixed-header');
-              $contentRegion.css('margin-top', '0');
-            }
-          });
-        }
-      }
-    };
+    var $preHeader, $header, $headerBar, $menu, $mobileMenuOpen, $regionHeader, $contentRegion, headerOffset, $window, $thisContext;
 
     function updateState() {
       $preHeader = $thisContext.find('.pre-header-wrapper');
@@ -39,21 +12,130 @@ window.BoatShows = window.BoatShows || {};
       $headerBar = $header.find('.header-bar');
       $contentRegion = $thisContext.find('.region.region-content');
       headerOffset = $header.offset().top;
-      console.log(headerOffset);
+      $menu = $header.find('.mega-menu');
+      $mobileMenuOpen = $header.find('.mobile-trigger');
+      $regionHeader = $header.find('.region-header');
     }
+
+    Drupal.behaviors.boatShowHeader = {
+      attach: function (context, settings) {
+        $thisContext = $(context);
+        $thisContext.find('header.header').once('boatshow-nav').each(function() {
+          updateState();
+
+          if ($(window).scrollTop() >= headerOffset) {
+            $header.addClass('fixed-header');
+            $contentRegion.css('margin-top', $headerBar.outerHeight());
+          }
+
+          if ($preHeader.outerHeight() > 0) {
+            // Attach window scroll event listener
+            $(window).scroll(function() {
+              if ($(window).scrollTop() >= headerOffset && !$header.hasClass('fixed-header')) {
+                $header.addClass('fixed-header');
+                $contentRegion.css('margin-top', $headerBar.outerHeight());
+              }
+              else if ($(window).scrollTop() < headerOffset && $header.hasClass('fixed-header')) {
+                $header.removeClass('fixed-header');
+                $contentRegion.css('margin-top', '0');
+              }
+            });
+          }
+
+          //
+          $menu.find('.parent-item').each(function() {
+            var $thisMenuItem = $(this);
+            var $thisSubMenu = $thisMenuItem.find('.menu-dropdown');
+
+            // Main nav triggers.
+            // var timer;
+            $thisMenuItem.hover(function() {
+              // mouse in
+              if ($(window).width() > 992) {
+                // clearTimeout(timer);
+                openSubmenu($thisSubMenu);
+              }
+
+            }, function() {
+              // mouseout
+              if ($(window).width() > 992) {
+                // NOTE: This timer doesn't actually work, but it's also not hurting
+                // anything, so I'm leaving it here for now.
+                // timer = setTimeout(closeSubmenu($(this).find('.menu-dropdown')), 1000);
+                // TODO: set timeout
+                closeSubmenu($thisSubMenu);
+              }
+            });
+          });
+
+          $mobileMenuOpen.click(function(event) {
+            event.preventDefault();
+
+            if (!$(context).find('body').hasClass('mobile-open')) {
+              if (!$header.hasClass('fixed-header')) {
+                $header.css('top', $header.offset().top + 'px');
+              }
+
+              $('body').addClass('mobile-open');
+              $regionHeader.css('padding-top', $headerBar.outerHeight());
+            }
+            else {
+              $('body').removeClass('mobile-open');
+              $header.css('top', 0);
+              $regionHeader.css('padding-top', 0);
+
+              if (BoatShows.hasOwnProperty('Header')) {
+                resetContentPadding();
+              }
+            }
+          });
+        });
+
+        $('#block-mainnavigationboatshow .mega-menu .parent-item > a').on('click', function(event) {
+          // Sometimes links are links and sometimes they're menu headers, and
+          // there's no way to tell them apart! As a workaround, disable preventDefault
+          // when
+          var $this = $(this);
+          var $submenu = $this.siblings('.menu-dropdown');
+          if ($submenu.length) {
+            event.preventDefault();
+          }
+
+          if ($(window).width() <= 992) {
+            if ($submenu.hasClass('open')) {
+              $submenu.removeClass('open');
+            }
+            else {
+              $submenu.addClass('open');
+            }
+          }
+        });
+      }
+    };
 
     function setStateFixed() {
       updateState();
       $header.addClass('fixed-header');
-      $contentRegion.css('margin-top', $header.outerHeight());
-      $window.off();
+      $header.css('top', 0);
+      $contentRegion.css('margin-top', $headerBar.outerHeight());
+      $(window).off();
     }
 
     function resetContentPadding() {
       updateState();
       if ($header.hasClass('fixed-header')) {
-        $contentRegion.css('margin-top', $header.outerHeight());
+        $contentRegion.css('margin-top', $headerBar.outerHeight());
       }
+    }
+
+    function openSubmenu($selector) {
+      $selector.siblings('a').addClass('open');
+      $selector.addClass('open');
+    }
+
+    function closeSubmenu($selector) {
+      $selector.siblings('a').removeClass('open');
+      $selector.removeClass('open');
     }
 
     return {
@@ -62,14 +144,6 @@ window.BoatShows = window.BoatShows || {};
       },
       resetContentPadding: function() {
         resetContentPadding();
-      },
-      getHeaderBarHeight: function() {
-        updateState();
-        return $headerBar.outerHeight();
-      },
-      getHeaderOffset: function() {
-        updateState();
-        return headerOffset;
       }
     };
   };
