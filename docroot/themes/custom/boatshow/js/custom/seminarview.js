47c11f8fc5259d94e648e25be9b3f7da32802796
Melinda Barr <mbarr@Melindas-MacBook-Pro-2.local>
2019-08-12 14:49:33 +0000

    [NMMA-83] removed whitespace from seminar dates on seminars page

---
+++ docroot/themes/custom/boatshow/js/custom/seminarview.js
@@ -0,0 +1,13 @@
+'use strict';
+
+(function ($, Drupal) {
+
+  Drupal.behaviors.boatshowSeminarView = {
+    attach: function attach(context, settings) {
+
+          $('.view-seminarreference .view-content .grouping-row').text(function (index, oldText) {
+            return oldText.replace(/^\s+|\s+$/g, '');
+          })
+     }
+  };
+})(jQuery, Drupal);
