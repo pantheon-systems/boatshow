e17b01d6d49b4b053fb376707fc5ea91a935402c
Max Gleeson <mgleeson@sandstormdesign.com>
2019-08-23 14:31:06 -0500

    Revert "Revert "loading object-fit-images from composer""
    This reverts commit 7eff9432ec5fa3742a504a701230dcca8ebfc804.
    
    Class Type filter for Seminars
    
    sail/power filter for seminars
    
    levels filter for seminars
    
    replaced entity filters with radios that popuplate fields. Styling begun
    
    patch to fix notices from collapsible BEF sections
    
    Custom brick type for exposed filters
    
    make seminar filters collapsible and in the correct order
    
    add in missing {{attributes}} to templates
    
    generalized styling for filter blocks.
    
    finish styling for exposed filters blocks
    
    apply text invert to title from field in exposed filters brick

--- docroot/themes/custom/boatshow/js/custom/featuresFilter.js
+++ docroot/themes/custom/boatshow/js/custom/featuresFilter.js
@@ -3,6 +3,74 @@
 (function($, Drupal) {
 
   // Example of Drupal behavior loaded.
+  Drupal.behaviors.boatshowCustomFilters = {
+    attach: function attach(context, settings) {
+      function dateToSQLstr(d){
+        let Y = d.getFullYear();
+        let M = (d.getMonth()+1).toString().padStart(2, '0');
+        let D = d.getDate().toString().padStart(2, '0');
+        return Y+'-'+M+'-'+D
+      }
+      // Convert a single date to min (00:00:00) and max(23:59:59) for a given day
+      // and set values to date-between form fields
+      function filterDateRangeCheckboxes(value, minTarget, maxTarget) {
+        let min = '';
+        let max = '';
+        if (value){
+          const d = new Date(value);
+          const date = dateToSQLstr(d);
+          min = date + ' 00:00:00';
+          max = date + ' 23:59:59';
+        }
+        jQuery(minTarget).val(min);
+        jQuery(maxTarget).val(max);
+      }
+
+      jQuery('input[type="radio"][name="field_date_radios"]').change(function() {
+        const value = jQuery(this).val();
+        const minTarget="#edit-field-smnr-seminar-date-value-min";
+        const maxTarget="#edit-field-smnr-seminar-date-value-max";
+        filterDateRangeCheckboxes(value, minTarget, maxTarget);
+      });
+
+      jQuery('input[type="radio"][name="field_speaker_radios"]').change(function() {
+        const value = jQuery(this).val();
+        jQuery('#edit-field-smnr-speaker-target-id').val(value);
+      });
+
+      jQuery('.brick--type--exposed-filters.collapsible .toggle-filters').click(function(){
+        const $form = jQuery(this).closest('.brick--type--exposed-filters.collapsible');
+        $form.toggleClass('expand');
+        $form.find('.form--inline >.form-item').each(function(){
+          if ($form.hasClass('expand') && jQuery(this).css('display') != 'none'){
+            jQuery(this).attr('open', '');
+          } else {
+            jQuery(this).removeAttr('open');
+          }
+        })
+      })
+
+      jQuery('.brick--type--exposed-filters .js-hide').each(function(){
+        if (jQuery(this).is(':only-child')){
+          jQuery(this).parent().remove();
+        } else {
+          jQuery(this).remove();
+        }
+
+      });
+
+      jQuery('.brick--type--exposed-filters.collapsible .close-filters').click(function(){
+        const $form = jQuery(this).closest('.brick--type--exposed-filters.collapsible');
+        $form.removeClass('expand');
+        $form.find('.form--inline >.form-item').each(function(){
+          jQuery(this).removeAttr('open');
+        })
+      })
+
+
+    }
+  };
+
   Drupal.behaviors.boatshowFeaturesFilter = {
     attach: function attach(context, settings) {
 
