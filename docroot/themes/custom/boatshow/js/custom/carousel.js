272c3d71b129bc77e6c250c642586b845b77a6ad
Eric Savage <esavage@sandstormdesign.com>
2019-08-28 16:39:18 -0500

    tweak carousel settings for dining

--- docroot/themes/custom/boatshow/js/custom/carousel.js
+++ docroot/themes/custom/boatshow/js/custom/carousel.js
@@ -36,10 +36,10 @@
           responsive: [
             {
               breakpoint: 768,
-              settings: {
-                infinite: false,
-                centerMode: false,
-              }
+              // settings: {
+              //   infinite: false,
+              //   centerMode: false,
+              // }
             },
             {
               breakpoint: 992,
