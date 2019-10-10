9d344af56e65329d8557ef0578b0d7caaf8f9250
dmarinnmmaorg <44479271+dmarinnmmaorg@users.noreply.github.com>
2019-10-10 13:02:34 -0500

    [NMMA-187] Seniority points (#53)
    # Conflicts:
    #	config/miami

---
+++ docroot/themes/custom/boatshow/js/custom/seniority-points.js
@@ -0,0 +1,20 @@
+(function ($, Drupal) {
+  'use strict';
+
+  Drupal.behaviors.seniorityPoints = {
+    attach: function attach(context, settings) {
+      $(context).find('.js-seniority-points-agreement').once('seniority-points-agreement').each(function() {
+        var $element = $(this);
+        $element.find("button").each(function() {
+          $(this).click(function() {
+            $element.hide();
+            $(context).find('.seniority-points').each(function() {
+              $(this).show();
+            });
+            $('html, body').animate({ scrollTop: 0 }, 0);
+          });
+        });
+      });
+    }
+  };
+})(jQuery, Drupal);
