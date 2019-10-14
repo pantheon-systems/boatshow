a6804edf8139117df4610683a0d5a4039e554134
Max Gleeson <mgleeson@sandstormdesign.com>
2019-10-14 15:31:55 -0500

    [NMMA-213] Use BLT multisites config to generate list of multisites to build during deployment

--- hooks/common/post-code-update/post-code-update.sh
+++ hooks/common/post-code-update/post-code-update.sh
@@ -3,6 +3,29 @@
 site="$1"
 target_env="$2"
 
+# Setup BLT alias
+function blt() {
+  if [[ ! -z ${AH_SITE_ENVIRONMENT} ]]; then
+    PROJECT_ROOT="/var/www/html/${AH_SITE_GROUP}.${AH_SITE_ENVIRONMENT}"
+  elif [ "`git rev-parse --show-cdup 2> /dev/null`" != "" ]; then
+    PROJECT_ROOT=$(git rev-parse --show-cdup)
+  else
+    PROJECT_ROOT="."
+  fi
+
+  if [ -f "$PROJECT_ROOT/vendor/bin/blt" ]; then
+    $PROJECT_ROOT/vendor/bin/blt "$@"
+
+  # Check for local BLT.
+  elif [ -f "./vendor/bin/blt" ]; then
+    ./vendor/bin/blt "$@"
+
+  else
+    echo "You must run this command from within a BLT-generated project."
+    return 1
+  fi
+}
+
 # Go to the path from where drush commands can be executed.
 repo_root="/var/www/html/$site.$target_env/docroot"
 #export PATH=$repo_root/vendor/bin:$PATH
@@ -10,7 +33,7 @@ cd $repo_root
 
 echo "New code has been deployed to $site.$target_env environment."
 
-for MULTISITE in "miami" "newyork"
+for MULTISITE in $(blt blt:config:get multisites)
 do
   echo "======== begin multisite: ${MULTISITE} ========"
 
