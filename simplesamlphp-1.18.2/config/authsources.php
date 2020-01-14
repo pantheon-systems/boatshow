7af18d82c6950362fa1c45de849c56bcb5b8c6fe
melyjean <melyjean@gmail.com>
2020-01-13 18:38:46 -0600

    updated acquiaconfig and authsources

--- simplesamlphp-1.18.2/config/authsources.php
+++ simplesamlphp-1.18.2/config/authsources.php
@@ -18,20 +18,12 @@ $config = [
 
         // The entity ID of this SP.
         // Can be NULL/unset, in which case an entity ID is generated based on the metadata URL.
-<<<<<<< HEAD
+
         'entityID' => 'okta-sso',
 
         // The entity ID of the IdP this SP should contact.
         // Can be NULL/unset, in which case the user will be shown a list of available IdPs.
         'idp' => 'http://www.okta.com/exk1i1y4rgaPKDkB90h8',
-=======
-
-        'entityID' => null,
-
-        // The entity ID of the IdP this SP should contact.
-        // Can be NULL/unset, in which case the user will be shown a list of available IdPs.
-        'idp' => null,
->>>>>>> origin/2.x-develop
 
         // The URL to the discovery service.
         // Can be NULL/unset, in which case a builtin discovery service will be used.
@@ -57,6 +49,10 @@ $config = [
             'urn:oid:x.x.x.x',
         ],
         */
+
+        'privatekey' => 'dev2.boatshownorwalk.saml.pem',
+        'certificate' => 'dev2.boatshownorwalk.saml.crt',
+        'signature.algorithm' => 'http://www.w3.org/2001/04/xmldsig-more#rsa-sha256',
     ],
 
 
