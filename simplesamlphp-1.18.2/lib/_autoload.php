ae64c0b8e7cc20601eceadb01c47d9a9b9d7d420
melyjean <melyjean@gmail.com>
2020-01-15 17:43:26 -0600

    autoload logging

--- simplesamlphp-1.18.2/lib/_autoload.php
+++ simplesamlphp-1.18.2/lib/_autoload.php
@@ -25,6 +25,7 @@ if (file_exists(dirname(dirname(__FILE__)) . '/vendor/autoload.php')) {
         require_once dirname(dirname(__FILE__)) . '/../../autoload.php';
     } elseif (file_exists(dirname(dirname(__FILE__)) . '/../vendor/autoload.php')) {
         require_once dirname(dirname(__FILE__)) . '/../vendor/autoload.php';
+        \Drupal::logger('my_module')->notice(dirname(dirname(__FILE__)) . '/../vendor/autoload.php');
     } else {
         throw new Exception('Unable to load Composer autoloader');
     }
