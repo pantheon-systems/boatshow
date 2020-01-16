d0cd513ef45ff63af2faa661504c2f2ba98ae0ec
melyjean <melyjean@gmail.com>
2020-01-16 17:41:37 -0600

    Feature 2.x develop nmma 240 sso blt install (#179)
    * initial commit
    
    * blt/blt.yml
    
    * more updates
    
    * more updates b
    
    * starting blt again
    
    * used instructions at acquia drupal pantheon blogpost
    
    * removed auto load from htaccess
    
    * added cert
    
    * updated acquiaconfig and authsources
    
    * tracing
    
    * tracing b
    
    * autoload directory if condition added
    
    * autoload logging
    
    * autoload update
    
    * autoload update removing drupal logger statements

--- simplesamlphp-1.18.2/lib/_autoload.php
+++ simplesamlphp-1.18.2/lib/_autoload.php
@@ -10,22 +10,15 @@
 
 // SSP is loaded as a separate project
 
-\Drupal::logger('mysimplesaml')->notice(dirname(dirname(__FILE__)) . '/vendor/autoload.php');
-\Drupal::logger('mysimplesaml')->notice(dirname(dirname(__FILE__)) . '/../../autoload.php');
-\Drupal::logger('mysimplesaml')->notice(dirname(dirname(__FILE__)) . '/../vendor/autoload.php');
-
 if (file_exists(dirname(dirname(__FILE__)) . '/vendor/autoload.php')) {
-    \Drupal::logger('my_module')->notice(dirname(dirname(__FILE__)) . '/vendor/autoload.php');
     require_once dirname(dirname(__FILE__)) . '/vendor/autoload.php';
 
 } else {
     // SSP is loaded as a library
     if (file_exists(dirname(dirname(__FILE__)) . '/../../autoload.php')) {
-      \Drupal::logger('my_module')->notice(dirname(dirname(__FILE__)) . '/../../autoload.php');
         require_once dirname(dirname(__FILE__)) . '/../../autoload.php';
     } elseif (file_exists(dirname(dirname(__FILE__)) . '/../vendor/autoload.php')) {
-        require_once dirname(dirname(__FILE__)) . '/../vendor/autoload.php';
-        \Drupal::logger('my_module')->notice(dirname(dirname(__FILE__)) . '/../vendor/autoload.php');
+            require_once dirname(dirname(__FILE__)) . '/../vendor/autoload.php';
     } else {
         throw new Exception('Unable to load Composer autoloader');
     }
