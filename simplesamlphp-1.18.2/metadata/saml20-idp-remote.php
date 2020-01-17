3e9958d76eaca972c8931aa58abe13ccaa253487
melyjean <melyjean@gmail.com>
2020-01-17 17:36:08 -0600

    IDP Drupal Show SSO Okta Application http restore 5

--- simplesamlphp-1.18.2/metadata/saml20-idp-remote.php
+++ simplesamlphp-1.18.2/metadata/saml20-idp-remote.php
@@ -7,7 +7,6 @@
  * See: https://simplesamlphp.org/docs/stable/simplesamlphp-reference-idp-remote
  */
 
-
  $metadata['http://www.okta.com/exk1hyssphtTGcbJB0h8'] = array (
    'entityid' => 'http://www.okta.com/exk1hyssphtTGcbJB0h8',
    'contacts' =>
