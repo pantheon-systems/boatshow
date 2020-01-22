5854482471314efb785517e5d5ed7b09a01237f6
melyjean <melyjean@gmail.com>
2020-01-22 12:28:07 -0600

    added new okta setup to use http instead of https

--- simplesamlphp-1.18.2/metadata/saml20-idp-remote.php
+++ simplesamlphp-1.18.2/metadata/saml20-idp-remote.php
@@ -126,3 +126,60 @@
      ),
    ),
  );
+ /**   Drupal Show SSO Okta  html **/
+ $metadata['http://www.okta.com/exk1i4uomd4iUVUft0h8'] = array (
+   'entityid' => 'http://www.okta.com/exk1i4uomd4iUVUft0h8',
+   'contacts' =>
+   array (
+   ),
+   'metadata-set' => 'saml20-idp-remote',
+   'SingleSignOnService' =>
+   array (
+     0 =>
+     array (
+       'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
+       'Location' => 'https://nmma.okta.com/app/nmma_drupalshowssooktahttp_1/exk1i4uomd4iUVUft0h8/sso/saml',
+     ),
+     1 =>
+     array (
+       'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+       'Location' => 'https://nmma.okta.com/app/nmma_drupalshowssooktahttp_1/exk1i4uomd4iUVUft0h8/sso/saml',
+     ),
+   ),
+   'SingleLogoutService' =>
+   array (
+   ),
+   'ArtifactResolutionService' =>
+   array (
+   ),
+   'NameIDFormats' =>
+   array (
+     0 => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
+   ),
+   'keys' =>
+   array (
+     0 =>
+     array (
+       'encryption' => false,
+       'signing' => true,
+       'type' => 'X509Certificate',
+       'X509Certificate' => 'MIIDmDCCAoCgAwIBAgIGAV2h3gMhMA0GCSqGSIb3DQEBCwUAMIGMMQswCQYDVQQGEwJVUzETMBEG
+ A1UECAwKQ2FsaWZvcm5pYTEWMBQGA1UEBwwNU2FuIEZyYW5jaXNjbzENMAsGA1UECgwET2t0YTEU
+ MBIGA1UECwwLU1NPUHJvdmlkZXIxDTALBgNVBAMMBG5tbWExHDAaBgkqhkiG9w0BCQEWDWluZm9A
+ b2t0YS5jb20wHhcNMTcwODAyMDczMzI3WhcNMjcwODAyMDczNDI3WjCBjDELMAkGA1UEBhMCVVMx
+ EzARBgNVBAgMCkNhbGlmb3JuaWExFjAUBgNVBAcMDVNhbiBGcmFuY2lzY28xDTALBgNVBAoMBE9r
+ dGExFDASBgNVBAsMC1NTT1Byb3ZpZGVyMQ0wCwYDVQQDDARubW1hMRwwGgYJKoZIhvcNAQkBFg1p
+ bmZvQG9rdGEuY29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA7js0fHpN2KoCNKJ1
+ rO3NEGw061einGMDSdKDQI6KaGVyzH1kBElIDSdeleHB8deHtoqAi5yhvy5t1tTB+6B878smtq3p
+ K/u4mMyNgBlVIpy7FdFmmuxw8iiU+SzMlHY7PjFXqUTfz6vv3/BzxOTffJ9QQzw3fv2oJl0M4j+g
+ DwUObXRieT4Oi90IlnYTUZRPS9o78WunydbQRrSkePZmRKYWwUO1lWyigHoJcrcRcZudzKVhVvEy
+ ENEulITGsW9MD3lMxT3H23HYqVgAw0yWvaHz3wbJ7rR6PjZgei+oMg0MzlnryOR42Z+AsBSQNOr5
+ gqUFTL00Phc61deSfU/HlQIDAQABMA0GCSqGSIb3DQEBCwUAA4IBAQAcQiGmqco2VmIvJIHfrMaQ
+ ZmJqpFVTrnCKXhAVAjXVyC2ofFWKicH6EqR32Sg8rBr+AiyhX7zU9Ti0L0kxXQ8p6OryTE5j1sdU
+ q9gjv9Gza0ftQ0OEpImcC5mdyyMFign/kbMs6adSH/zD+e6oaEzMxj5UF+Mzq4sFLI1zFN3SUo5c
+ PNzSKS2MI9Dnc5bvM8mfWBv0zkl6HgBf6X993gJAfJ28vvJ1Kj79NH5WEnrqLYykc1sPhplyY1/0
+ OJarLacqJXdi+l//gL/uT/0OA3TA3I4JPV7qbiPQKJuiUXGNNiy0TkZTkLcmZOH/2k/YbIQU0o1K
+ IGVezl2GnehhLt7m',
+     ),
+   ),
+ );
