5d1ac10c11f463367ef5851e9958da3a026d79f1
melyjean <melyjean@gmail.com>
2020-02-03 19:44:12 -0600

    protocol set to 443

--- simplesamlphp-1.18.2/config/config.php
+++ simplesamlphp-1.18.2/config/config.php
@@ -33,7 +33,7 @@ $config = [
      */
      //mjb
     //'baseurlpath' => 'simplesaml/',
-    'baseurlpath' => 'https://' . $_SERVER['HTTP_HOST'] . '/simplesaml/';
+    'baseurlpath' => 'https://' . $_SERVER['HTTP_HOST'] . ':443/simplesaml/';
 
     /*
      * The 'application' configuration array groups a set configuration options
